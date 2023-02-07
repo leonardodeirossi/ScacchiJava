d4c578565ea0d0594dbda18b0904a394832b020f
Mattia Andreolli <mattia.andreolli1@gmail.com>
2023-02-07 15:57:05 +0100

    Fixed Position class
    Now stores as int, and has isValid() implemented

--- src/Position.java
+++ src/Position.java
@@ -2,24 +2,34 @@ package src;
 
 public class Position {
     private int row;
-    private char col;
+    private int col;
 
     public Position(int row, char col) {
+        if(!isValid(row, col))
+        {
+            throw new IllegalArgumentException("Invalid position \"" + row + col + "\"");
+        }
+
         this.row = row;
-        this.col = col;
+
+        //If col is above 97 it is lowercase, subtract 32 to get uppercase
+        if(col >= 97) col -= 32;
+        this.col = col - 64;    //Subtract 64 to get actual 1-8 number (uppercase A is 65)
     }
 
     public int getRow() {
         return this.row;
     }
+    public int getCol() { return this.col; }
 
-    public char getCol() {
-        return this.col;
+    public char getColChar() {
+        return (char) (this.col + 64);
     }
 
     public void setPosition(int row, char col) throws Exception {
         if (isValid(row, col)) {
-            setPosition(new Position(row, col));
+            this.row = row;
+            this.col = col;
         } else {
             throw new Exception("invalid position");
         }
@@ -31,14 +41,23 @@ public class Position {
     }
 
     public boolean isValid() {
-        return true; // To be implemented
+        return isValid(row, getColChar());
     }
 
     public boolean isValid(int row, char col) {
-        return true; // To be implemented
+        return (row > 0 && row <= 8)    //Row must be between 1 and 8 (inclusive)
+                && ((col >= 97 && col <= 104) || (col >= 65 && col <= 72)); //Column must be between 'A' and 'H' or 'a' and 'h' (inclusive)
     }
 
-    public boolean equals(Position pos) {
-        return this.row == pos.row && this.col == pos.col;
+    public boolean equals(Position pos) { return this.row == pos.row && this.col == pos.col; }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof Position && equals((Position) obj);
+    }
+
+    @Override
+    public String toString() {
+        return "" + row + col;
     }
 }
